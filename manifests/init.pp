39c675f2b95ae5c0d7832072a25f59b865f8523a
Andrew Brader <abrader@gmail.com>
2016-11-07 10:05:58 -0500

    Moved repo_url to public HTTP repo url

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,51 +1,10 @@
-# Class: plexpy
-# ===========================
-#
-# Full description of class plexpy here.
-#
-# Parameters
-# ----------
-#
-# Document parameters here.
-#
-# * `sample parameter`
-# Explanation of what this parameter affects and what it defaults to.
-# e.g. "Specify one or more upstream ntp servers as an array."
-#
-# Variables
-# ----------
-#
-# Here you should define a list of variables that this module would require.
-#
-# * `sample variable`
-#  Explanation of how this variable affects the function of this class and if
-#  it has a default. e.g. "The parameter enc_ntp_servers must be set by the
-#  External Node Classifier as a comma separated list of hostnames." (Note,
-#  global variables should be avoided in favor of class parameters as
-#  of Puppet 2.6.)
-#
-# Examples
-# --------
-#
-# @example
-#    class { 'plexpy':
-#      servers => [ 'pool.ntp.org', 'ntp.local.company.com' ],
-#    }
-#
-# Authors
-# -------
-#
-# Andrew Brader <abrader@gmail.com>
-#
-# Copyright
-# ---------
-#
-# Copyright 2016 Your name here, unless otherwise noted.
-#
+# PlexPy Module
+
 class plexpy (
   String $basedir   = '/opt/plexpy',
   String $configdir = '/etc/plexpy',
   String $user      = 'plexpy',
+  String $repo_url  = 'https://github.com/JonnyWong16/plexpy.git',
   Boolean $latest   = true,
 )  {
 
@@ -58,7 +17,7 @@ class plexpy (
     vcsrepo { $basedir :
       ensure   => present,
       provider => git,
-      source   => 'git@github.com:JonnyWong16/plexpy.git',
+      source   => $repo_url,
       revision => 'master',
     }
   }
@@ -66,7 +25,7 @@ class plexpy (
     vcsrepo { $basedir :
       ensure   => present,
       provider => git,
-      source   => 'git@github.com:JonnyWong16/plexpy.git',
+      source   => $repo_url,
       owner    => $user,
       group    => $user,
     }
