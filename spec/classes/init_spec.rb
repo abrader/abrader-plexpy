4f7d644d9551f2333c8af6675f43bb66b50cdb0c
Andrew Brader <abrader@gmail.com>
2018-01-18 14:32:15 -0500

    Updated core unit test

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -1,7 +1,33 @@
-require 'spec_helper'
-describe 'plexpy' do
+# Main test class
 
+require 'spec_helper'
+
+describe 'plexpy' do
   context 'with defaults for all parameters' do
-    it { should contain_class('plexpy') }
+    it { is_expected.to compile.with_all_deps }
+
+    it { is_expected.to contain_class('plexpy') }
+
+    describe 'plexpy' do
+      it { is_expected.to contain_user('plexpy') }
+
+      it { is_expected.to contain_vcsrepo('/opt/plexpy').with_owner('plexpy') }
+      it { is_expected.to contain_vcsrepo('/opt/plexpy').with_group('plexpy') }
+      it { is_expected.to contain_vcsrepo('/opt/plexpy').with_source('https://github.com/JonnyWong16/plexpy.git') }
+      it { is_expected.to contain_vcsrepo('/opt/plexpy').with_revision('master') }
+
+      it { is_expected.to contain_file('/opt/plexpy').with_owner('plexpy') }
+      it { is_expected.to contain_file('/opt/plexpy').with_group('plexpy') }
+
+      it { is_expected.to contain_file('/etc/plexpy').with_owner('plexpy') }
+      it { is_expected.to contain_file('/etc/plexpy').with_group('plexpy') }
+
+      it { is_expected.to contain_file('/usr/lib/systemd/system/plexpy.service').with_owner('root') }
+      it { is_expected.to contain_file('/usr/lib/systemd/system/plexpy.service').with_group('root') }
+      it { is_expected.to contain_file('/usr/lib/systemd/system/plexpy.service').with_mode('0644') }
+
+      it { is_expected.to contain_service('plexpy').with_ensure('running') }
+      it { is_expected.to contain_service('plexpy').with_ensure('true') }
+    end
   end
 end
